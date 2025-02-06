5b445f389dc317356a741395b836559733fc1d1f
MrZeexy <tauzia31@gmail.com>
2025-02-06 10:48:28 +0100

    Resources css update

--- js/pages/views/resourcePageView.js
+++ js/pages/views/resourcePageView.js
@@ -1,13 +1,13 @@
  
 export function resourcePageView() {
     return `<h1>Ressurser</h1>
-    <p>
+    <p class="rssP">
     Her finner du informasjon om Hub Phønix som organisasjon.
     </p>
 
-    <p>
-        <h2>Sammendrag av vedtekter</h2>
-        <p>
+    <p class="rssP1">
+        <h2 class="rssH"">Sammendrag av vedtekter</h2>
+        <p class="rssP2">
             Phønix er en demokratisk-styrt medlemsorganisasjon. Det ble stiftet mandag 10. juni, 2024.
             Formålet med foreningen er å bygge et kompetansesenter etter FNs bærekraftmål i regionen, og det er hvorfor logoet har alle fargene den har.
 
